c3dde8e38dda8334a457326198e3795b19efb94e
karl.kranich <karl.kranich@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-05 20:04:49 +0000

    First version of plugin
    git-svn-id: https://plugins.svn.wordpress.org/visual-verse-of-the-day-widget/trunk@1039055 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,55 @@
+=== Visual Verse of the Day Widget ===
+Contributors: karl.kranich
+Donate link: http://karl.kranich.org
+Tags: widget
+Requires at least: 3.0.1
+Tested up to: 4.0.1
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Plugin for adding a widget for [The Visual Verse of the Day](http://visualverse.thecreationspeaks.com/).
+
+== Description ==
+
+This plugin adds a widget for [The Visual Verse of the Day](http://visualverse.thecreationspeaks.com/).
+It dynamically grabs the latest image and verse reference.
+
+== Installation ==
+
+1. Find the plugin in the Wordpress plugin gallery and click "Install Now".
+2. In the Wordpress dashboard, go to Appearance > Widgets.
+3. Find the new Visual Verse of the Day widget and click on it.
+4. Choose the desired widget area and click "Add Widget".
+5. Find the list of widgets for that widget area and drag them into the desired order.
+
+== Frequently Asked Questions ==
+
+= What is the "width" setting for? =
+
+If you don't want the widget to be the full width of the widget area, set a width in CSS style (like 200px; or 80%;).
+
+= Can I style it more? =
+
+Sure, find and edit the file wp-content/plugins/vv-widget/css/vv-widget-styles.css.
+
+== Screenshots ==
+
+1. Here is an example:
+
+== Changelog ==
+
+= 1.1 =
+Changed text below image from picture title to verse reference.
+
+= 1.0 =
+First version.
+
+== Upgrade Notice ==
+
+= 1.1 =
+Upgrade recommended to get the verse reference.
+
+= 1.0 =
+First version.
+
---
+++ vv-widget.php
@@ -0,0 +1,93 @@
+<?php
+/**
+Plugin Name: Visual Verse of the Day Widget
+Description: Plugin for adding a widget for the Visual Verse of the Day from http://visualverse.thecreationspeaks.com/
+Version: 1.1
+Author: Karl Kranich
+Author URI: http://karl.kranich.org/visual-verse-widget
+*/
+// Creating the widget
+class vv_widget extends WP_Widget {
+	function __construct() {
+		parent::__construct('vv_widget',__('Visual Verse of the Day Widget', 'vv_widget_domain'),
+		array( 'description' => __( 'Display the latest image and verse reference from Visual Verse of the Day', 'vv_widget_domain' ), ));
+	}
+ 
+	// Creating widget front-end
+	// This is where the action happens
+	public function widget( $args, $instance ) {
+		// Register the styles
+    	wp_register_style('vv-widget-styles', plugins_url('/css/vv-widget-styles.css', __FILE__ ), false, null);
+    	wp_enqueue_style('vv-widget-styles');
+    	// Get the width from saved settings
+		$width = apply_filters( 'widget_width', $instance['width'] );
+		$width = trim($width);
+		if ($width == '') { $width = '100%'; }
+		if (substr($width, -1) != ';') { $width .= ';'; }
+		// before and after widget arguments are defined by themes
+		echo $args['before_widget'];
+		// main output here
+		$rssfeed = simplexml_load_file('http://visualverse.thecreationspeaks.com/feed/');
+
+		$i = 0;
+		while ($i < 5) {
+			$vv_title = $rssfeed->channel[0]->item[$i]->title;
+			$vv_link = $rssfeed->channel[0]->item[$i]->link;
+			if (substr($vv_title,0,4) != 'Blog') break;
+			$i++;
+		}
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_AUTOREFERER, TRUE);
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_URL, $vv_link);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);       
+		$page_data = curl_exec($ch);
+		curl_close($ch);
+
+		preg_match('@<div class="entry-content.*<img .*src="(http://visual\S*\.jpg)"@s', $page_data, $matches);
+		$img_link = $matches[1];
+		preg_match('/<meta name="description" content="([^&]*) &quot/', $page_data, $matches);
+		$verse_ref = $matches[1];
+		?>
+	  	<div id="vv-widget" style="width: <?php echo $width; ?>">
+	  		<h3 class="widget-title" id="vv-title">Visual Verse of the Day</h3>
+	  		<a href="http://visualverse.thecreationspeaks.com/" target="_blank"><img src="<?php echo $img_link; ?>"></a>
+	      <p id="vv-picTitle"><?php echo $verse_ref; ?></p>
+	  	</div>
+	  	<?php
+
+		echo $args['after_widget'];
+	}
+	         
+	// Widget Backend
+	public function form( $instance ) {
+		if ( isset( $instance[ 'width' ] ) ) {
+		$width = $instance[ 'width' ];
+	} else {
+		$width = __( '', 'vv_widget_domain' );
+	}
+
+	// Widget admin form
+	?>
+	<p>
+	<label for="<?php echo $this->get_field_id( 'width' ); ?>"><?php _e( 'Width: Use CSS-type syntax, like "100px;" or leave empty for 100%'); ?></label>
+	<input class="widefat" id="<?php echo $this->get_field_id( 'width' ); ?>" name="<?php echo $this->get_field_name( 'width' ); ?>" type="text" value="<?php echo esc_attr( $width ); ?>" />
+	</p>
+	<?php
+	}
+	     
+	// Updating widget replacing old instances with new
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['width'] = ( ! empty( $new_instance['width'] ) ) ? strip_tags( $new_instance['width'] ) : '';
+		return $instance;
+	}
+} // Class wpb_widget ends here
+
+// Register and load the widget
+function vv_load_widget() {
+    register_widget( 'vv_widget' );
+}
+add_action( 'widgets_init', 'vv_load_widget' );
